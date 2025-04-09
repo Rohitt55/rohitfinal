41a3f28e7eaf6b2543e7ad61259c5bd8261b4872
rohit <rohitt5531@gmail.com>
2025-04-09 23:58:32 +0600

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'screens/welcome_screen.dart';
+import 'screens/login_screen.dart';
+import 'screens/register_screen.dart';
+import 'screens/home_screen.dart';
 import 'screens/profile_screen.dart';
 import 'screens/statistics_screen.dart';
 import 'screens/transaction_screen.dart';
 import 'screens/add_transaction_screen.dart';
-import 'screens/home_screen.dart';
-import 'screens/welcome_screen.dart';
-import 'screens/login_screen.dart';
-import 'screens/register_screen.dart';
 
 void main() {
   runApp(const ExpenseApp());
@@ -26,24 +28,57 @@ class ExpenseApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: const EntryPoint(),
       routes: {
+        '/welcome': (context) => const WelcomeScreen(),
+        '/login': (context) => const LoginScreen(),
+        '/register': (context) => const RegisterScreen(),
         '/home': (context) => const HomeScreen(),
         '/profile': (context) => const ProfileScreen(),
         '/statistics': (context) => const StatisticsScreen(),
         '/transactions': (context) => const TransactionScreen(),
         '/add': (context) => const AddTransactionScreen(),
-        '/welcome': (context) => const WelcomeScreen(),
-        '/login': (context) => const LoginScreen(),
-        '/register': (context) => const RegisterScreen(),
       },
     );
   }
 }
 
-class EntryPoint extends StatelessWidget {
+class EntryPoint extends StatefulWidget {
   const EntryPoint({super.key});
 
   @override
+  State<EntryPoint> createState() => _EntryPointState();
+}
+
+class _EntryPointState extends State<EntryPoint> {
+  Widget? _startScreen;
+
+  @override
+  void initState() {
+    super.initState();
+    _initApp();
+  }
+
+  void _initApp() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isFirstTime = prefs.getBool('isFirstTime') ?? true;
+    bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
+
+    if (isFirstTime) {
+      await prefs.setBool('isFirstTime', false);
+      _startScreen = const WelcomeScreen();
+    } else if (isLoggedIn) {
+      _startScreen = const HomeScreen();
+    } else {
+      _startScreen = const LoginScreen();
+    }
+
+    setState(() {});
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const WelcomeScreen();
+    if (_startScreen == null) {
+      return const Scaffold(body: Center(child: CircularProgressIndicator()));
+    }
+    return _startScreen!;
   }
 }
